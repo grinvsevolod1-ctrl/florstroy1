ed4684233121c99e32f92920946b073e4e1ed6f6
Всеволод <grinvsevolod1@netnext.site>
2025-09-20 19:11:16 +0300

    123

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,11 +18,13 @@ import { NewsletterModalContextProvider, useNewsletterModalContext } from 'conte
 import { NavItems } from 'types';
 
 const navItems: NavItems = [
-  { title: 'Awesome SaaS Features', href: '/features' },
-  { title: 'Pricing', href: '/pricing' },
-  { title: 'Contact', href: '/contact' },
-  { title: 'Sign up', href: '/sign-up', outlined: true },
+  { title: 'Проекты', href: '/features' },
+  { title: 'Услуги', href: '/pricing' },
+  { title: 'Контакты', href: '/contact' },
+  { title: 'Оставить заявку', href: '/sign-up', outlined: true },
 ];
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
