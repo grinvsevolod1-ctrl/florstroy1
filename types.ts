effdd7124af54626313343a8a48cb4e89b74fe92
Всеволод <grinvsevolod1@netnext.site>
2025-09-21 11:35:39 +0300

    123

--- types.ts
+++ types.ts
@@ -19,3 +19,21 @@ export type NonNullableChildren<T> = { [P in keyof T]: Required<NonNullable<T[P]
 export type NonNullableChildrenDeep<T> = {
   [P in keyof T]-?: NonNullableChildrenDeep<NonNullable<T[P]>>;
 };
+declare module 'react-input-mask' {
+  import * as React from 'react';
+
+  interface InputMaskProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    mask: string;
+    alwaysShowMask?: boolean;
+    maskChar?: string | null;
+    beforeMaskedValueChange?: (
+      newState: { value: string; selection: { start: number; end: number } },
+      oldState: { value: string; selection: { start: number; end: number } },
+      userInput: string,
+      maskOptions: any
+    ) => { value: string; selection: { start: number; end: number } };
+  }
+
+  const InputMask: React.FC<InputMaskProps>;
+  export default InputMask;
+}
