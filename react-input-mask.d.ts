4e7db5a2ce257e898b2eaed93689d636c5dbac00
Всеволод <grinvsevolod1@netnext.site>
2025-09-21 11:38:43 +0300

    еуые

---
+++ react-input-mask.d.ts
@@ -0,0 +1,18 @@
+declare module 'react-input-mask' {
+  import * as React from 'react';
+
+  interface InputMaskProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    mask: string;
+    alwaysShowMask?: boolean;
+    maskChar?: string | null;
+    beforeMaskedValueChange?: (
+      newState: { value: string; selection: { start: number; end: number } },
+      oldState: { value: string; selection: { start: number; end: number } },
+      userInput: string,
+      maskOptions: any
+    ) => { value: string; selection: { start: number; end: number } };
+  }
+
+  const InputMask: React.FC<InputMaskProps>;
+  export default InputMask;
+}
