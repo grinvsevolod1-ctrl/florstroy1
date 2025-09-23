039ff55fdea4a761417bbb0bbe6a2577d32dbb99
Всеволод <grinvsevolod1@netnext.site>
2025-09-23 15:03:42 +0300

    123

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,8 @@ import 'swiper/css/autoplay';
 
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
-import React, { PropsWithChildren, useEffect } from 'react';
+import { ColorModeScript } from 'nextjs-color-mode';
+import React, { PropsWithChildren } from 'react';
 
 import Footer from 'components/Footer';
 import { GlobalStyle } from 'components/GlobalStyles';
@@ -29,10 +30,6 @@ const navItems: NavItems = [
 ];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    document.body.classList.add('next-light-theme');
-  }, []);
-
   return (
     <>
       <Head>
@@ -40,7 +37,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
-
+      <ColorModeScript />
       <GlobalStyle />
 
       <Providers>
