b68dc180dddadaee31586f85b05895eab333f25f
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-28 12:54:35 +0300

    Update CartIcon.tsx

--- components/CartIcon.tsx
+++ components/CartIcon.tsx
@@ -10,7 +10,7 @@ export default function CartIcon() {
   return (
     <CartWrapper onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
       <Icon>
-        <svg viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+        <svg viewBox="0 0 24 24" fill="currentColor">
           <path d="M7 4h-2l-1 2h2l3.6 7.59-1.35 2.44c-.16.28-.25.61-.25.97 0 1.1.9 2 2 2h12v-2h-12l1.1-2h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.3.12-.48 0-.55-.45-1-1-1h-16zm0 16c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
         </svg>
         {totalItems > 0 && <Badge>{totalItems}</Badge>}
@@ -22,18 +22,29 @@ export default function CartIcon() {
 
 const CartWrapper = styled.div`
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Icon = styled.div`
   position: relative;
   width: 2.4rem;
   height: 2.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   color: rgb(var(--text));
   cursor: pointer;
 
   svg {
     width: 100%;
     height: 100%;
+  }
+
+  @media (max-width: 768px) {
+    width: 2rem;
+    height: 2rem;
   }
 `;
 
@@ -48,4 +59,11 @@ const Badge = styled.span`
   padding: 0.2rem 0.5rem;
   border-radius: 1rem;
   line-height: 1;
+
+  @media (max-width: 768px) {
+    font-size: 0.8rem;
+    padding: 0.1rem 0.4rem;
+    top: -0.4rem;
+    right: -0.4rem;
+  }
 `;
