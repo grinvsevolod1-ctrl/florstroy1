f9897a83e43250b43784f213116a06916f2e85ee
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-28 18:53:19 +0300

    Update checkout.tsx

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { PropsWithChildren, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { NavItems } from 'types';
+import ClientOnly from './ClientOnly';
 import CloseIcon from './CloseIcon';
 import OriginalDrawer from './Drawer';
 
