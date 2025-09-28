084277e1998f1a74b283edef5ff055e5cbb49026
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-28 07:42:28 +0300

    Update material.tsx

--- pages/material.tsx
+++ pages/material.tsx
@@ -1,1 +1,20 @@
-gh
+import styled from 'styled-components';
+import Page from 'components/Page';
+import MaterialGrid from 'views/MaterialPage/MaterialGrid';
+
+export default function MaterialPage() {
+  return (
+    <Page
+      title="Материалы для бетонных полов"
+      description="Витрина материалов Florstroy: топпинги, эпоксидные составы, упрочнители, гидроизоляция. Закажите напрямую без регистрации."
+    >
+      <Wrapper>
+        <MaterialGrid />
+      </Wrapper>
+    </Page>
+  );
+}
+
+const Wrapper = styled.div`
+  padding-bottom: 15rem;
+`;
