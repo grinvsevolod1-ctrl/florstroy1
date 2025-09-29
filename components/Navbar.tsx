13ab2b902f373c8b10b3661948b2842ad84f729c
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-29 11:35:10 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -104,6 +104,24 @@ export default function Navbar({ items }: NavbarProps) {
             )}
           </NavItemList>
           <RightSide>
+            <DesktopOnly>
+              <ContactInfo>
+                <ContactLine>
+                  <ContactIcon viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M20 4H4a2 2 0 00-2 2v12a2 2 0 002 2h16a2 2 0 002-2V6a2 2 0 00-2-2zm0 2v.01L12 13 4 6.01V6h16zM4 18V8l8 5 8-5v10H4z" />
+                  </ContactIcon>
+                  <ContactLabel>Email:</ContactLabel>
+                  <ContactValue>info@florstroy.ru</ContactValue>
+                </ContactLine>
+                <ContactLine>
+                  <ContactIcon viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21c1.21.49 2.53.76 3.88.76a1 1 0 011 1v3.5a1 1 0 01-1 1C10.07 22 2 13.93 2 4a1 1 0 011-1h3.5a1 1 0 011 1c0 1.35.26 2.67.76 3.88a1 1 0 01-.21 1.11l-2.43 2.8z" />
+                  </ContactIcon>
+                  <ContactLabel>Телефон:</ContactLabel>
+                  <ContactValue>+7 965 168-63-58</ContactValue>
+                </ContactLine>
+              </ContactInfo>
+            </DesktopOnly>
             <IconGroup>
               <IconCircle>
                 <ColorSwitcher />
@@ -225,10 +243,6 @@ const DropdownMenu = styled.ul`
       color: rgb(var(--text));
       font-weight: 500;
     }
-
-    &:hover {
-      background: rgba(var(--primary), 0.1);
-    }
   }
 `;
 
@@ -280,7 +294,22 @@ const MobileOnly = styled.div`
     display: none;
   }
 `;
-// Reusable contact styles (если используешь в NavigationDrawer или отдельном компоненте)
+
+const DesktopOnly = styled.div`
+  ${media('<desktop')} {
+    display: none;
+  }
+`;
+
+const ContactInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+  font-size: 1.3rem;
+  color: rgb(var(--text));
+  text-align: left;
+  min-width: 240px;
+`;
 
 const ContactLine = styled.div`
   display: flex;
