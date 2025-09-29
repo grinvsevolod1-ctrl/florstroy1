722734abec745e5d0ca9b4b527ae5f05937c5e0a
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-29 08:14:57 +0300

    Update equipment.tsx

--- pages/equipment.tsx
+++ pages/equipment.tsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+import Page from 'components/Page';
 import EquipmentGrid from 'views/EquipmentPage/EquipmentGrid';
 
 export default function EquipmentPage() {
-  return <EquipmentGrid items={equipmentItems} />;
+  return (
+    <Page
+      title="Оборудование для бетонных полов"
+      description="Витрина оборудования Florstroy: затирочные машины, пылесосы, миксеры, лазерные нивелиры. Закажите напрямую без регистрации."
+    >
+      <Wrapper>
+        <EquipmentGrid />
+      </Wrapper>
+    </Page>
+  );
 }
+
+const Wrapper = styled.div`
+  padding-bottom: 15rem;
+`;
