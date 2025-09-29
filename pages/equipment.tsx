9acde40eebdff4821efddc4c149379fe52ed5e39
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-29 08:05:19 +0300

    Update equipment.tsx

--- pages/equipment.tsx
+++ pages/equipment.tsx
@@ -1,20 +1,6 @@
-import styled from 'styled-components';
-import Page from 'components/Page';
 import EquipmentGrid from 'views/EquipmentPage/EquipmentGrid';
+import { equipmentItems } from 'data/equipment';
 
 export default function EquipmentPage() {
-  return (
-    <Page
-      title="Оборудование для бетонных полов"
-      description="Витрина оборудования Florstroy: затирочные машины, шлифовальные системы, дозаторы, инструменты. Закажите напрямую без регистрации."
-    >
-      <Wrapper>
-        <EquipmentGrid />
-      </Wrapper>
-    </Page>
-  );
+  return <EquipmentGrid items={equipmentItems} />;
 }
-
-const Wrapper = styled.div`
-  padding-bottom: 15rem;
-`;
