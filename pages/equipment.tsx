ad822baacdd87efeee6610ef58221c5cb308e37f
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-29 08:12:51 +0300

    Update equipment.tsx

--- pages/equipment.tsx
+++ pages/equipment.tsx
@@ -1,5 +1,4 @@
 import EquipmentGrid from 'views/EquipmentPage/EquipmentGrid';
-import { equipmentItems } from 'data/equipment';
 
 export default function EquipmentPage() {
   return <EquipmentGrid items={equipmentItems} />;
