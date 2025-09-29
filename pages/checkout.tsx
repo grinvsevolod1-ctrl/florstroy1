4d488046c969332342ba184d134addfc5f1dc44a
grinvsevolod1-ctrl <grin.vsevolod1@gmail.com>
2025-09-29 13:20:44 +0300

    Update checkout.tsx

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import { useCartContext } from 'context/CartContext';
+
 
 export default function Checkout() {
   const { cart, totalPrice } = useCartContext();
